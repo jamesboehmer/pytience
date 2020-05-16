c67f5e061aad7214a938309a4a41ca171a3acec9
James Boehmer <james.boehmer@gmail.com>
2020-05-16 12:23:16 -0400

    First commit

---
+++ test/test_klondike_game.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from pytience.games.solitaire.klondike import KlondikeGame  # pylint: disable=unused-import
+
+
+class KlondikeGameTestCase(TestCase):
+    def test_create(self):
+        pass  # TODO: implement
+
+    def test_deal(self):
+        pass  # TODO: implement
+
+    def test_tableau_move(self):
+        pass  # TODO: implement
+
+    def test_tableau_to_foundation(self):
+        pass  # TODO: implement
+
+    def test_waste_to_tableau(self):
+        pass  # TODO: implement
+
+    def test_waste_to_foundation(self):
+        pass  # TODO: implement
+
+    def test_foundation_to_tableau(self):
+        pass  # TODO: implement
+
+    def test_undo(self):
+        pass  # TODO: implement
+
